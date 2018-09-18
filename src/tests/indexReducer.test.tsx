db12e4d26b377f8069a6d54d2b99c3e99ad617f8
kayra <kayraalat@gmail.com>
2018-09-18 16:15:34 +0100

    Refactor

--- src/tests/indexReducer.test.tsx
+++ src/tests/indexReducer.test.tsx
@@ -19,12 +19,11 @@ it("Increases the enthusiasm level in the state by one when passed the INCREMENT
     languageName: "",
     enthusiasmLevel: 3
   }
-
   const actualResultState = enthusiasm(initialTestState, incrementEnthusiasm());
-
   expect(actualResultState).toEqual(expectedResultState);
 
 });
+
 
 it("Decreases the enthusiasm level in the state by one when passed the DECREMENT_ENTHUSIASM type", () => {
   
@@ -32,12 +31,11 @@ it("Decreases the enthusiasm level in the state by one when passed the DECREMENT
     languageName: "",
     enthusiasmLevel: 1
   }
-
   const actualResultState = enthusiasm(initialTestState, decrementEnthusiasm());
-
   expect(actualResultState).toEqual(expectedResultState);
 
 });
+
 
 it("Does not allow the enthusiasm level in the state to fall below 1", () => {
   
