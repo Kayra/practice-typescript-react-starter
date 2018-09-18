74a70c64ffb8addedb88a13f3f936548c5e82626
kayra <kayraalat@gmail.com>
2018-09-18 16:44:48 +0100

    Added initial test structure for redux hello component

---
+++ src/tests/components_ReduxHello.test.tsx
@@ -0,0 +1,13 @@
+import { enthusiasm } from "../reducers/index";
+import { StoreState } from "../types/index";
+
+import Hello from "../components/ReduxHello";
+
+
+it("Triggers onIncrement when the + button is clicked", () => {
+
+});
+
+it("Triggers onDecrement when the - button is clicked", () => {
+
+});
