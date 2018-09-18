560a6c71912bd5ba080d2a2741c821ec944c2306
kayra <kayraalat@gmail.com>
2018-09-18 15:34:21 +0100

    Added test structure

---
+++ src/tests/indexReducer.test.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import * as enzyme from "enzyme";
+
+import enthusiasm from "../reducers/index";
+
+let testState = {
+  languageName = "",
+  enthusiasmLevel = 1
+}
+
+it("Increases the enthusiasm level in the state by one when passed the INCREMENT_ENTHUSIASM type", () => {
+
+});
+
+it("Decreases the enthusiasm level in the state by one when passed the DECREMENT_ENTHUSIASM type", () => {
+  
+});
+
+it("Does not allow the enthusiasm level in the state to fall below 1", () => {
+  
+});
