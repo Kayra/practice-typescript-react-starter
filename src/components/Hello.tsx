8e60e41dc75b92ba1a43e61e4515234a8a03c680
kayra <kayraalat@gmail.com>
2018-09-19 10:58:51 +0100

    Updated hello component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -6,13 +6,15 @@ import "./Hello.css";
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
 }
 
 function getExclamationMarks(numberOfCharacters: number) {
   return Array(numberOfCharacters + 1).join("!");
 }
 
-function Hello({name, enthusiasmLevel = 1}: Props) {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
 
   if (enthusiasmLevel <= 0 ) {
     throw new Error("The enthusiasm level can't be below zero.");
@@ -21,9 +23,17 @@ function Hello({name, enthusiasmLevel = 1}: Props) {
   return (
 
     <div className="hello">
+
       <div className="greeting">
         <p>Hello {name + getExclamationMarks(enthusiasmLevel)}</p>
       </div>
+
+      <div>
+        <button className="increment" onClick={onIncrement}>+</button>
+        <button className="decrement" onClick={onDecrement}>-</button>
+      </div>
+
+
     </div>
 
   );
