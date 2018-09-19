d892037dcb5aef9820defb67f7a5992b4ce480d3
kayra <kayraalat@gmail.com>
2018-09-19 10:25:30 +0100

    Updated index to use container

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import StatefulHello from "./components/StatefulHello";
+import Hello from "./containers/Hello";
+import { enthusiasm } from "./reducers/index";
+// import { StoreState } from "./types/index";
+
+
+const store = createStore(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "Typescript"
+});
 
 
 ReactDOM.render(
-  <StatefulHello name="Typescript" enthusiasmLevel={1} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
